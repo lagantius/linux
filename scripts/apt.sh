fcf92610aa0ee8df00f054ddbeb3906c125b1609
lagantius <157399706+lagantius@users.noreply.github.com>
2025-09-02 20:29:54 +0200

    Update apt.sh

--- scripts/apt.sh
+++ scripts/apt.sh
@@ -3,7 +3,7 @@
 MINIMAL=$1
 
 ########## MINIMAL ##########
-sudo apt install sway swaybg swaylock swayimg waybar noto-fonts noto-fonts-cjk ttf-dejavu noto-fonts-emoji vim alacritty cliphist flatpak yazi rofi-wayland brightnessctl fastfetch autotiling base-devel picom go grim slurp xorg-xwayland mpv librewolf -y
+sudo apt install sway swaybg swaylock swayimg waybar noto-fonts noto-fonts-cjk ttf-dejavu noto-fonts-emoji vim alacritty cliphist flatpak yazi rofi-wayland brightnessctl fastfetch autotiling base-devel picom go grim slurp xorg-xwayland flatpak mpv librewolf -y
 
 ########## EXTRA ##########
 if [ "$MINIMAL" = "true" ]; then
