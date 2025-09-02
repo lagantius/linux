120b3943d0f7164f900c6d9c017153bb20ebaff6
lagantius <157399706+lagantius@users.noreply.github.com>
2025-09-02 20:14:40 +0200

    Update zypper.sh

--- scripts/zypper.sh
+++ scripts/zypper.sh
@@ -3,11 +3,11 @@
 MINIMAL=$1
 
 ########## MINIMAL ##########
-sudo zypper in sway swaybg swaylock swayimg waybar google-noto-fonts google-noto-fonts-cjk dejavu-fonts google-noto-fonts-emoji vim alacritty cliphist flatpak yazi rofi-wayland brightnessctl fastfetch autotiling-rs picom grim slurp xwayland mpv opi -y
+sudo zypper -n in sway swaybg swaylock swayimg waybar google-noto-fonts google-noto-fonts-cjk dejavu-fonts google-noto-fonts-emoji vim alacritty cliphist flatpak yazi rofi-wayland brightnessctl fastfetch autotiling-rs picom grim slurp xwayland mpv opi
 opi librewolf -n
 
 ########## EXTRA ##########
 if [ "$MINIMAL" = "true" ]; then
 	echo "Pokračuji v minimální instalaci"
 elif [ "$MINIMAL" = "false" ]; then
-	sudo zypper in steam lutris chromium btop -y
+	sudo zypper -n in steam lutris chromium btop
