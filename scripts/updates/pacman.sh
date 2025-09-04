c221c206d91ac4d97775e73a8cd8f40928bf5978
lagantius <157399706+lagantius@users.noreply.github.com>
2025-09-04 19:56:39 +0200

    Update pacman.sh

--- scripts/updates/pacman.sh
+++ scripts/updates/pacman.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-sudo echo "==========> AKTUALIZACE SYSTÉMU <=========="
+sudo echo ""
+echo "==========> AKTUALIZACE SYSTÉMU <=========="
 yay -Syu --noconfirm
+echo ""
 
+echo ""
 echo "==========> AKTUALIZACE FLATPAKŮ <=========="
 flatpak update -y
+echo ""
 
+echo ""
 echo "==========> ODSTRAŇOVÁNÍ BALÍKŮ <=========="
 flatpak remove --unused
 yay -Rns $(pacman -Qdtq)
 yay -Scc --noconfirm
+echo ""
 
+echo ""
 echo "==========> AKTUALIZACE HOTOVY <=========="
+echo ""
