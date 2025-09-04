71ac38c99f27ac637964c414ab783d72d0f3dc4d
lagantius <157399706+lagantius@users.noreply.github.com>
2025-09-04 22:31:05 +0200

    Update flatpak.sh

--- scripts/flatpak.sh
+++ scripts/flatpak.sh
@@ -8,12 +8,12 @@ flatpak override --user --env=GTK_THEME=Adwaita-dark
 flatpak override --user --env=QT_QPA_PLATFORMTHEME=qt5ct
 flatpak override --user --env=QT_STYLE_OVERRIDE=Adwaita-Dark
 
-flatpak install flathub com.github.calo001.luna com.github.PintaProject.Pinta org.gnome.Calculator -y
+flatpak install flathub com.github.calo001.luna com.infinipaint.infinipaint org.gnome.Calculator -y
 
 ########## EXTRA ##########
 if [ "$MINIMAL" = "true" ]; then
 	echo "Pokračuji v minimální instalaci"
 elif [ "$MINIMAL" = "false" ]; then
-	flatpak install flathub dev.vencord.Vesktop io.mgba.mGBA net.kuribo64.melonDS org.azahar_emu.Azahar com.bitwarden.desktop -y
+	flatpak install flathub dev.vencord.Vesktop io.mgba.mGBA net.kuribo64.melonDS org.azahar_emu.Azahar com.bitwarden.desktop com.obsproject.Studio org.gimp.GIMP -y
 	flatpak override --user --filesystem=~/obrázky dev.vencord.Vesktop
 fi
