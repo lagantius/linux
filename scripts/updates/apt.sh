14d2c03ab0c635aa4aced35bc9e9646f8d7b7320
lagantius <157399706+lagantius@users.noreply.github.com>
2025-09-04 19:55:45 +0200

    Update apt.sh

--- scripts/updates/apt.sh
+++ scripts/updates/apt.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
+echo ""
 sudo echo "==========> AKTUALIZACE SYSTÉMU <=========="
 sudo apt update -y
 sudo apt full-upgrade -y
+echo ""
 
+echo ""
 echo "==========> AKTUALIZACE FLATPAKŮ <=========="
 flatpak update -y
+echo""
 
+echo ""
 echo "==========> ODSTRAŇOVÁNÍ BALÍKŮ <=========="
 flatpak remove --unused
 sudo apt autoremove -y
 sudo apt clean -y
+echo ""
 
+echo ""
 echo "==========> AKTUALIZACE HOTOVY <=========="
+echo ""
