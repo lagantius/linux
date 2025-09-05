a34279b15c55d006df79d8eca01562268efff773
lagantius <157399706+lagantius@users.noreply.github.com>
2025-09-05 19:03:00 +0200

    Update pacman.sh

--- scripts/pacman.sh
+++ scripts/pacman.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 MINIMAL=$1
-sudo -v
 
 ########## MINIMAL ##########
 sudo pacman -S sway swaybg swaylock swayimg waybar noto-fonts noto-fonts-cjk ttf-dejavu noto-fonts-emoji vim alacritty cliphist flatpak yazi rofi-wayland brightnessctl fastfetch autotiling base-devel picom go grim slurp xorg-xwayland nmtui mpv --noconfirm
