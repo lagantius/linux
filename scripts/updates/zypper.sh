322b0b647d0b5fc5ce3919a155a93df0e6aa3088
lagantius <157399706+lagantius@users.noreply.github.com>
2025-09-05 18:10:02 +0200

    Update zypper.sh

--- scripts/updates/zypper.sh
+++ scripts/updates/zypper.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
-sudo echo ""
+sudo -v
+echo ""
 echo "==========> AKTUALIZACE SYSTÉMU <=========="
 sudo zypper -n refresh
 sudo zypper -n dup
-echo ""
 
 echo ""
 echo "==========> AKTUALIZACE FLATPAKŮ <=========="
 flatpak update -y
-echo ""
 
 echo ""
 echo "==========> ODSTRAŇOVÁNÍ BALÍKŮ <=========="
 flatpak remove --unused
+
+orphans=$(zypper packages --orphaned | awk '/^i/ {print $5}' || true)
+if [[ -n "$orphans" ]]; then
+	sudo zypper -n rm $orphans
+fi
+
 sudo zypper -n clean --all
-sudo zypper -n rm $(zypper packages --orphaned | awk '/^i/ {print $5}')
-echo ""
 
 echo ""
 echo "==========> AKTUALIZACE HOTOVY <=========="
-echo ""
