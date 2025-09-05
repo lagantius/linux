f9dd712d8e781c3418556d8d44815d1d51a7f3bf
lagantius <157399706+lagantius@users.noreply.github.com>
2025-09-05 21:34:03 +0200

    Update apt.sh

--- scripts/apt.sh
+++ scripts/apt.sh
@@ -3,7 +3,7 @@
 MINIMAL=$1
 
 ########## MINIMAL ##########
-sudo apt install sway swaybg swaylock swayimg waybar fonts-noto fonts-noto-cjk fonts-dejavu-core noto-fonts-color-emoji vim alacritty cliphist flatpak mc rofi-wayland brightnessctl fastfetch autotiling picom grim slurp xwayland nmtui mpv extrepo -y
+sudo apt install sway swaybg swaylock swayimg waybar fonts-noto fonts-noto-cjk fonts-dejavu-core noto-fonts-color-emoji vim alacritty cliphist flatpak mc rofi-wayland brightnessctl fastfetch autotiling picom grim slurp xwayland mpv extrepo -y
 sudo extrepo enable librewolf
 sudo apt update
 sudo apt install librewolf
