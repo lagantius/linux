d0ffdac10160be1afaa1eb93c6598dedcab46e36
lagantius <157399706+lagantius@users.noreply.github.com>
2025-09-07 14:44:57 +0200

    Update zypper.sh

--- scripts/zypper.sh
+++ scripts/zypper.sh
@@ -4,7 +4,7 @@ MINIMAL=$1
 
 ########## MINIMAL ##########
 sudo zypper -n in sway swaybg swaylock swayimg waybar google-noto-fonts google-noto-sans-cjk-fonts dejavu-fonts google-noto-coloremoji-fonts vim alacritty cliphist flatpak mc rofi-wayland brightnessctl fastfetch autotiling-rs picom grim slurp xwayland mpv opi
-opi librewolf -n
+sudo opi librewolf -n
 
 ########## EXTRA ##########
 if [ "$MINIMAL" = "true" ]; then
