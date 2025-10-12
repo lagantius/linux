fe35cc8f34a424198274c7638d4c3d11f99495ca
lagantius <157399706+lagantius@users.noreply.github.com>
2025-10-12 18:53:48 +0200

    Update pacman.sh

--- scripts/pacman.sh
+++ scripts/pacman.sh
@@ -3,7 +3,7 @@
 MINIMAL=$1
 
 ########## MINIMAL ##########
-sudo pacman -S sway swaybg swaylock swayimg waybar noto-fonts noto-fonts-cjk ttf-dejavu noto-fonts-emoji vim alacritty cliphist flatpak mc rofi-wayland brightnessctl fastfetch autotiling base-devel picom go grim slurp xorg-xwayland mpv qt5ct nwg-look --noconfirm
+sudo pacman -S sway swaybg swaylock swayimg waybar noto-fonts noto-fonts-cjk ttf-dejavu noto-fonts-emoji vim alacritty cliphist flatpak mc rofi-wayland brightnessctl fastfetch autotiling base-devel picom go grim slurp xorg-xwayland mpv qt5ct qt6ct qt6 nwg-look --noconfirm
 
 git clone https://aur.archlinux.org/yay-bin.git
 cd yay-bin
